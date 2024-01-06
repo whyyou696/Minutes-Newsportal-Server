d6656f3e12897e07ad2006bc1ee3dab9fc777da0
whyyou696 <whyyousick696@gmail.com>
2024-01-06 18:54:42 +0700

    feat:deploying

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== "production")
-//environtmet variables
-require("dotenv").config()
+if (process.env.NODE_ENV !== "production"){
+    require("dotenv").config()
+}
 //console.log(process.env)
 const express = require('express');
 const router = require('./routes');
