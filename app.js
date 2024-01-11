6b614cc754506f05b8aa29bc5f8b15ebdea0b528
whyyou696 <whyyousick696@gmail.com>
2024-01-11 12:21:40 +0700

    cors

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ if (process.env.NODE_ENV !== "production"){
 //console.log(process.env)
 const express = require('express');
 const router = require('./routes');
-
 const app = express()
+const cors = require("cors") // require dulu cors nya
+app.use(cors()) // tambahkan ini pada app kalian
 // middlewares body-parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()) // body-parser : JSON
