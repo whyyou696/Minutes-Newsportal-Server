fd37b55d1c89dc673687b52c534e43b3ebfe1f3c
whyyou696 <whyyousick696@gmail.com>
2024-01-12 10:47:25 +0700

    fix:seeders hashpassword

--- seeders/20240102112546-users.js
+++ seeders/20240102112546-users.js
@@ -1,10 +1,12 @@
 'use strict';
 const fs = require('fs')
+const { hashPassword } = require("../helpers/bcrypt");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const data = JSON.parse(fs.readFileSync('./data/user.json', 'utf-8')).map(el => {
-      el.username, el.email, el.password, el.role, el.phoneNumber, el.address, el.createdAt = el.updatedAt = new Date();
+    let data = JSON.parse(fs.readFileSync('./data/user.json', 'utf-8')).map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      el.password = hashPassword(el.password);
       return el
     })
     await queryInterface.bulkInsert('Users', data, {})
